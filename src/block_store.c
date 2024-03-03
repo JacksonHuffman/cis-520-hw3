60c8f3b2029cd5c90b1a6bdd96065d5f3641c94c
Jackson Huffman <jdhuffman02@ksu.edu>
2024-03-03 11:38:39 -0600

    Finished Checkpoint 1 Implementation(s)

--- src/block_store.c
+++ src/block_store.c
@@ -2,30 +2,89 @@
 #include <stdint.h>
 #include "../include/bitmap.h"
 #include "../include/block_store.h"
+
 // include more if you need
+#include <string.h>
 
 // You might find this handy.  I put it around unused parameters, but you should
 // remove it before you submit. Just allows things to compile initially.
 #define UNUSED(x) (void)(x)
 
-/// Defining the block_store struct
-struct block_store
+// Defining block_t struct
+// Contains 1 field
+// The field is an array of unit8_t of size BLOCK_SIZE_BYTES
+typedef struct block
 {
-    size_t id;
-    bitmap_t *map;
-};
+    uint8_t Block[BLOCK_SIZE_BYTES];
+} block_t;
 
+// Defining block_store_t struct
+// Contains 2 fields
+// First field is a pointer to a bitmap
+// Second field is an array of block_t types of size BLOCK_SOTRE_NUM_BLOCKS
+typedef struct block_store
+{
+    bitmap_t *map;
+    block_t blocks[BLOCK_STORE_NUM_BLOCKS];
+} block_store_t;
+
+///
+/// This creates a new BS device, ready to go
+/// \return Pointer to a new block storage device, NULL on error
+///
 block_store_t *block_store_create()
 {
-    return NULL;
+    // Declare and assign a block_store_t pointer using malloc
+    block_store_t *bs = (block_store_t*)malloc(sizeof(block_store_t));
+
+    // Check if the block_store_t pointer was incorrectly allocated
+    // Return null
+    if(!bs)
+    {
+        return NULL;
+    }
+    
+    // Zero out bs
+    memset(bs, 0, sizeof(block_store_t));
+
+    // Set bitmap field of bs to an overlay using bitmap_overlay
+    bs->map = bitmap_overlay(BITMAP_SIZE_BITS, &(bs->blocks[BITMAP_START_BLOCK]));
+
+    /* This implementation is for when I implement block_store_request
+    for(size_t i = BITMAP_START_BLOCK; i < BITMAP_START_BLOCK + BITMAP_NUM_BLOCKS; i++)
+    {
+        if(!block_store_request(bs, i))
+        {
+            //TODO: Handle error & break
+            fprintf(stderr,"*** Could not allocate block %lu for the bitmap ***\n",i);
+            break;
+        }
+    }
+    */
+
+   // Return pointer to block_store_t pointer
+    return bs;
 }
 
+///
+/// Destroys the provided block storage device
+/// This is an idempotent operation, so there is no return value
+/// \param bs BS device
+///
 void block_store_destroy(block_store_t *const bs)
 {
     // First check if bs is not NULL
     // If so, continue to free fields and eventually the bs pointer
     if(bs)
     {
+        // Check if bitmap field of bs is not NULL
+        if(bs->map)
+        {
+            // If so, free that memory
+            free(bs->map);
+        }
+
+        // Free pointer to bs
         free(bs);
     }
 }
@@ -37,6 +96,12 @@ size_t block_store_allocate(block_store_t *const bs)
 
 bool block_store_request(block_store_t *const bs, const size_t block_id)
 {
+    /*
+    if(bs && block_id <= BLOCK_STORE_NUM_BLOCKS)
+    {
+
+    }
+    */
     UNUSED(bs);
     UNUSED(block_id);
     return false;
